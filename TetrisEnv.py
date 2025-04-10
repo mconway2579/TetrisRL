bd57d04c71d766618902087c21c975fed6add401
max <maxlconway@gmail.com>
2025-04-10 15:46:38 -0600

    added time frames

--- TetrisEnv.py
+++ TetrisEnv.py
@@ -5,37 +5,61 @@ import numpy as np
 import torch.nn.functional as F
 
 
+def process_default_observation(self, obs):
+    #TODO
+    #Use conv instead of nested loop
+
+    obs = obs[27:203, 24:63]
+    #cv2.imshow("old_obs", obs)
+    #obs = cv2.resize(obs, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
+    obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY)
+    obs[obs==111] = 0
+    #obs[obs!=0] = 255
+    #print(obs)
+    row_centers = list(range(4, obs.shape[0], 8))
+    col_centers = list(range(1, obs.shape[1], 4))
+
+    new_obs = np.zeros((len(row_centers) , len(col_centers)), dtype=obs.dtype)
+    for ni, i in enumerate(row_centers):
+        for nj, j in enumerate(col_centers):
+            #obs[i,j] = [255, 0, 255]
+            new_obs[ni, nj] = 255 if obs[i,j] != 0 else 0
+    return new_obs
 
 
 class TetrisEnv(gym.Env):
     def __init__(self):
         super(TetrisEnv, self).__init__()
-        self.env = gym.make('ALE/Tetris-v5', render_mode='rgb_array', repeat_action_probability=0, frameskip = 4)
+        self.env = gym.make('ALE/Tetris-v5', render_mode='rgb_array', repeat_action_probability=0, frameskip = 8)
         self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
-        self.last_observation = None
+        self.obs_t1 = None
+        self.obs_t2 = None
+        self.obs_t3 = None
 
     def reset(self):
         self.env.reset()
-        return self.step(0)
+        obs_t1, reward, terminated, truncated, info = self.env.step(0)
+        obs_t2, reward, terminated, truncated, info = self.env.step(0)
+        obs_t3, reward, terminated, truncated, info = self.env.step(0)
+        self.obs_t1 = process_default_observation(self, obs_t1)
+        self.obs_t2 = process_default_observation(self, obs_t2)
+        self.obs_t3 = process_default_observation(self, obs_t3)
+        return self.get_observation()
 
+    def get_observation(self):
+        stacked_image = np.stack([self.obs_t1, self.obs_t2, self.obs_t3], axis=-1)
+        return stacked_image
+    
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
-        obs = obs[27:203, 22:64]
-        #obs[obs==111] = 0
-        #obs[obs!=0] = 255
-        for i in range(obs.shape[0]):
-            if (i+0) % 8 == 0:
-                    obs[i,:, 1] = 255
-        for j in range(obs.shape[1]):
-            if (j-1) % 4 == 0:
-                obs[:, j, 2] = 255
+        new_obs = process_default_observation(self, obs)
+        self.obs_t1 = self.obs_t2
+        self.obs_t2 = self.obs_t3
+        self.obs_t3 = new_obs
 
-        #obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY)
-        
-        
-        self.last_observation = obs
-        return obs, reward, terminated, truncated, info 
+        self.last_observation = self.get_observation()
+        return self.last_observation, reward, terminated, truncated, info 
 
     def render(self, mode='human'):
         return self.env.render(mode)
@@ -48,13 +72,27 @@ if __name__ == "__main__":
     env = TetrisEnv()
     obs = env.reset()
     done = False
+    action = env.action_space.sample()  # Replace with your policy or agent's action
     while not done:
-        action = env.action_space.sample()  # Replace with your policy or agent's action
         obs, reward, terminated, truncated, info = env.step(action)
         print(f"Action: {action}, {type(action)}")
         print(f"Observation: {obs.shape}, {type(obs)}")
         cv2.imshow("Tetris", obs)
-        cv2.waitKey(1)
+        k = cv2.waitKey(0) & 0xFF
+        # Check which key was pressed.
+        if k == ord('w'):
+            action = 1
+        elif k == ord('d'):
+            action = 2
+        elif k == ord('a'):
+            action = 3
+        elif k == ord('s'):
+            action = 4
+        elif k == ord('q'):
+            env.close()
+            exit()
+        else:
+            action = 0
         done = terminated or truncated
     cv2.waitKey(0)
     env.close()
